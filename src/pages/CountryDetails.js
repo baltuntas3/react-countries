f9bac23bdb439a2b11b78c55059338f5229fea99
burak <burak.altuntas@yandex.com>
2023-03-06 01:45:37 +0300

    Fix CSS

--- src/pages/CountryDetails.js
+++ src/pages/CountryDetails.js
@@ -31,69 +31,70 @@ export default function CountryDetails() {
     return (
         <>
             <PageHeader></PageHeader>
-
-            <div className="btn-wrapper margin-between-components">
-                <div className="back-btn-container">
-                    <button className="back-btn">
-                        <Link className="btn-link" to="/">
-                            <svg className="back-btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
-                            </svg>
-                            <p className="btn-text">Back</p>
-                        </Link>
-                    </button>
+            <div className="wrapper-container">
+                <div className="btn-wrapper margin-between-components">
+                    <div className="back-btn-container">
+                        <button className="back-btn">
+                            <Link className="btn-link" to="/">
+                                <svg className="back-btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+                                </svg>
+                                <p className="btn-text">Back</p>
+                            </Link>
+                        </button>
+                    </div>
                 </div>
-            </div>
 
-            {!loading ? (
-                <div className="div90">
+                {!loading ? (
                     <div className="details-container">
-                        <div className="flag-side">
+                        <div className="detail-column">
                             <img className="flag" alt="bayrak" src={countryDetails?.flags?.svg}></img>
                         </div>
-                        <div className="details-side">
-                            <div className="details-title">
-                                <strong>Title</strong>
-                            </div>
-                            <div className="column1">
-                                <p className="country-detail">
-                                    <strong>Native Name(s):</strong>
-                                </p>
-                                <p className="country-detail">
-                                    <strong>Population:</strong> {countryDetails?.population}
-                                </p>
-                                <p className="country-detail">
-                                    <strong>Region:</strong> {countryDetails?.region}
-                                </p>
-                                <p className="country-detail">
-                                    <strong>Sub Region:</strong>
-                                </p>
-                                <p className="country-detail">
-                                    <strong>Capital:</strong>
-                                </p>
-                            </div>
-                            <div className="column2">
-                                <p className="country-detail">
-                                    <strong>Top Level Domain:</strong>
-                                </p>
-                                <p className="country-detail">
-                                    <strong>Currencies:</strong>
-                                </p>
-                                <p className="country-detail">
-                                    <strong>Languages:</strong>
-                                </p>
-                            </div>
-                            <div className="">
-                                <p className="country-detail">
-                                    <strong>Borders:</strong>
-                                </p>
+                        <div className="detail-column">
+                            <div className="details-side">
+                                <div className="details-title">
+                                    <strong>Title</strong>
+                                </div>
+                                <div className="column1">
+                                    <p className="country-detail">
+                                        <strong>Native Name(s):</strong>
+                                    </p>
+                                    <p className="country-detail">
+                                        <strong>Population:</strong> {countryDetails?.population}
+                                    </p>
+                                    <p className="country-detail">
+                                        <strong>Region:</strong> {countryDetails?.region}
+                                    </p>
+                                    <p className="country-detail">
+                                        <strong>Sub Region:</strong>
+                                    </p>
+                                    <p className="country-detail">
+                                        <strong>Capital:</strong>
+                                    </p>
+                                </div>
+                                <div className="column2">
+                                    <p className="country-detail">
+                                        <strong>Top Level Domain:</strong>
+                                    </p>
+                                    <p className="country-detail">
+                                        <strong>Currencies:</strong>
+                                    </p>
+                                    <p className="country-detail">
+                                        <strong>Languages:</strong>
+                                    </p>
+                                </div>
+                                <div className="">
+                                    <p className="country-detail">
+                                        <strong>Borders:</strong>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            ) : (
-                <div>Yükleniyor...</div>
-            )}
+                ) : (
+                    <div>Yükleniyor...</div>
+                )}
+            </div>
         </>
     );
 }
